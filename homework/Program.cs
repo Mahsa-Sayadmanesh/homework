8bbba2c2dd5c7ae8084e6b98f1b9bb8ed5bb39f8
Mahsa Sayadmanesh <MHSH@simcorp.com>
2022-11-23 14:25:16 +0100

    HW2 (assignment 1&2)

--- homework/Program.cs
+++ homework/Program.cs
@@ -5,8 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace homework {
-    class Program {
+    public class Program {
         static void Main(string[] args) {
+
+            Console.WriteLine("Input the nominal of the trade:");
+            double Nominal = double.Parse(Console.ReadLine() ?? "1");
+            Console.WriteLine("Give the transaction type:");
+            string userInput = Console.ReadLine();
+            TransactionType trcType = (TransactionType)Enum.Parse(typeof(TransactionType), userInput, true);
+            int Sign = (trcType == TransactionType.Sell) ? -1 : 1;
+            Console.WriteLine("Enter security price:");
+            double Price = double.Parse(Console.ReadLine());
+            double CurrentValue = Sign * Price * Nominal;
+            Console.WriteLine($"Here is the details of your trade:\n Nominal: {Nominal}\n Price: {Price}\n Transaction:{trcType}\nCurrent Value:{CurrentValue}");
+            //Assignment 2
+            Console.WriteLine("Enter the original price of this instrument:");
+            double OriginalPrice = double.Parse(Console.ReadLine());
+            double ProfitLoss = trcType == TransactionType.Buy ? 0 : (Price - OriginalPrice) * Nominal;
+            Console.WriteLine($"Calculated Profit/loss is:{ ProfitLoss}");
+            Console.ReadKey();
+
+
         }
     }
 }
